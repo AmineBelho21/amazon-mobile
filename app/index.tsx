2db759119ac13869f8f827b63792679afee17b85
AmineBelho21 <amine.belhocine@fastdelivery.dz>
2025-10-15 12:32:29 +0100

    Add API URL configuration and update app settings
    - Added EXPO_PUBLIC_API_URL to the .env file for API endpoint configuration.
    - Updated app.json to allow arbitrary loads for iOS.
    - Enhanced index.tsx to fetch and display articles from the API, incorporating state management and error handling.

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+
+  const [data, setData] = useState<any>(null);
+  const API_URL = process.env.EXPO_PUBLIC_API_URL;
+
+  useEffect(() => {
+    fetch(`${API_URL}/articles`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setData(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View className="flex-1 justify-center items-center">
       <Text className="color-red-500">Edit tshehe to edit this screen.</Text>
+      {data && <Text className="color-blue-500">{JSON.stringify(data)}</Text>}
     </View>
   );
 }
