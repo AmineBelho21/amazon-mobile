0ec7f204a2ffb7af83e404725d5a0a0f5acc2187
AmineBelho21 <amine.belhocine@fastdelivery.dz>
2025-10-16 14:15:57 +0100

    Add Clerk authentication integration and update dependencies
    - Integrated Clerk for user authentication in the app.
    - Added sign-in screen and updated profile page to prompt for sign-in.
    - Updated app.json and package.json to include new dependencies: @clerk/clerk-expo, expo-build-properties, and expo-secure-store.
    - Refactored _layout.tsx to include ClerkProvider and handle authentication state.

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,23 @@
+import { SignedOut } from '@clerk/clerk-expo'
+import { Link } from 'expo-router'
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 const Page = () => {
   return (
-    <View>
-      <Text>profile</Text>
+    <View className='flex-1'>
+        <SignedOut>
+            <View className='pt-10 px-8 items-center'>
+                <Text className="text-3xl text-center">Sign in for the optimal experience</Text>
+            </View>
+            <View className="mt-8 w-full px-8">
+          <Link href="/signIn" asChild>
+            <TouchableOpacity className="bg-primary py-3 px-4 border border-dark">
+              <Text className="text-center text-base text-dark">Sign in</Text>
+            </TouchableOpacity>
+          </Link>
+        </View>
+        </SignedOut>
     </View>
   )
 }
